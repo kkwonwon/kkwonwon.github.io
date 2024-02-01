a5667e4d56ef12dba67389f3b0997233bdf48a5a
kimwon <104348387+kkwonwon@users.noreply.github.com>
2024-02-02 03:34:33 +0900

    read file setting

--- src/file.ts
+++ src/file.ts
@@ -1,2 +1,20 @@
-const fileDir: string= "../photos";
+const folderPath= "./photos/";
 import fs = require("fs");
+import path = require("path");
+import EXIF = require("exif-js");
+let photos: Array<string> = [];
+
+fs.readdirSync(folderPath).map(folderName => {
+    const newPath = path.join(folderPath, folderName);
+    // console.log(folderName);
+
+    fs.readdirSync(newPath).map(fileName => {
+        photos.push(path.join(newPath, fileName));
+        // console.log(fileName);
+    });
+});
+
+photos.map(s => {
+    console.log(s);
+})
+
